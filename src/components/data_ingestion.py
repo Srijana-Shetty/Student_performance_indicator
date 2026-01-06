cb622e6db3509d1e4f0b15c0b66f59637c65845d
Srijana-Shetty <srujanashetty434@gmail.comgit config --global user.name>
2026-01-06 19:34:49 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -17,7 +19,6 @@ class DataIngestionConfig:
 
 class DataIngestion:
     def __init__(self):
-        # ✅ fixed spelling
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
@@ -70,4 +71,8 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+
+    data_transformation =DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
